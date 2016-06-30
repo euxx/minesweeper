192300e9a37f72936aa83b6b556f90cc32b00ee0
eux <ixue@163.com>
2016-06-30 16:10:13 +0800

    Initial files

---
+++ scripts/script.js
@@ -0,0 +1,28 @@
+
+$(function() {
+
+	display();
+	render();
+	$(".grid").click(play);
+
+});
+
+
+function display() {
+	const cont = $(".container")
+	cont.append("<h4>Have Fun^</h4>");
+}
+
+function render(gridNum) {
+	$(".container").append("<div class='box'></div>");
+	const box = $(".box");
+	let i = 0;
+	for ( ; i < gridNum; i++) {
+		box.append("<div class='row'></div>");
+	}
+	const row = $(".row");
+	i = 0;
+	for ( ; i < gridNum; i++) {
+		row.append("<div class='grid' /*tabindex='0'*/></div>");
+	}
+}
